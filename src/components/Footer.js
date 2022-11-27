e0d6b1af694627b50f67bd88603b34fb5bf77bbb
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-11-27 01:05:27 -0600

    footer listo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import "../css-components/footer-style.css";
 
 const Footer = () => {
     return (
@@ -9,8 +10,8 @@ const Footer = () => {
                     <div class="row text-center">
                         <div style={{ textAlign: "center" }}>
                             <marquee direction="left" SCROLLDELAY="30">
-                                <img 
-                                    src="Images/carro.png"
+                                <img
+                                    src="react-example/Images/carro.png"
                                     title="Logo"
                                     alt="Logo del Proyecto"
                                     width="130"
@@ -20,12 +21,13 @@ const Footer = () => {
                         <h4 class="mb-4 font-weight-bold text-uppercase">Contactos</h4>
                         <ul class="list-group">
                             <li class="list-group-item bg-transparent border-0 p-0 mb-2">
-                                <a href="https://www.google.com/intl/es-419/gmail/about/"
+                                <a
+                                    href="https://www.google.com/intl/es-419/gmail/about/"
                                     title="gmail"
                                     target="_blank"
                                     style={{ marginRight: 35 + "px" }}>
                                     <img
-                                        src="/gmail.png"
+                                        src="react-example/Images/gmail.png"
                                         title="Gmail"
                                         alt="Gmail"
                                         width="45"
@@ -36,7 +38,7 @@ const Footer = () => {
                                     title="facebook"
                                     target="_blank">
                                     <img
-                                        src="../Images/facebook.png"
+                                        src="react-example/Images/facebook.png"
                                         title="Facebook"
                                         alt="Facebook"
                                         width="45"
@@ -46,6 +48,38 @@ const Footer = () => {
                         </ul>
                     </div>
                 </div>
+                
+                <h4 style={{
+                    textAlign: "center",
+                    marginTop: "15" + "px"
+                }}>¿Quiénes somos?</h4>
+                <p style={{
+                        textAlign:"center",
+                        marginTop: "15" + "px"
+                    }} >Somos un negocio electrónico especializado en la venta de carros modificados con motores eléctricos, los cuales
+                        ofrecen una autonomía de 250 kilómetros, además visto desde el cuidado del ambiente, estos vehículos generan menos emisiones de CO2.
+                    </p>
+
+                <div>
+                    <ul class="list-group" style={{ textAlign: "center" }}>
+                        <li class="list-group-item bg-transparent border-0 p-0 mb-2"
+                            style={{ fontSize: "20" + "px" }}>
+                            <img
+                                src="react-example/Images/Cookie.png"
+                                title="cookie"
+                                alt="cookie"
+                                width="35"
+                                height="35" />
+                            <a
+                                href="http://www.google.com/intl/es-419/policies/technologies/types/"
+                                title="gmail"
+                                target="_blank"><i
+                                    class="fa fa-google-plus mr-1"></i>Cookies</a>
+
+                        </li>
+                    </ul>
+                </div>
+
 
                 <br></br>
             </footer>
@@ -56,18 +90,7 @@ const Footer = () => {
 
 /*
 
-                <h4 style={{ 
-                    textAlign: "center", 
-                    marginTop: "15" + "px" 
-                    }}>¿Quiénes somos?</h4>
-                <p style={{ 
-                    textAlign: "center", 
-                    marginLeft:"300"+"px",
-                    marginRight:"300"+"px",
-                    marginBottom:"20"+"px",
-                    }} >Somos un negocio electrónico especializado en la venta de carros modificados con motores eléctricos, los cuales
-                    ofrecen una autonomía de 250 kilómetros, además visto desde el cuidado del ambiente, estos vehículos generan menos emisiones de CO2.
-                </p>
+                
                 <ul class="list-group" style={{ textAlign: "center" }}>
                     <li class="list-group-item bg-transparent border-0 p-0 mb-2" 
                     style={{fontSize:"20"+"px"}}>
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,12 +12,13 @@ const Home = () => {
                     <div class="col text-center">
                         <img
                             className="img-fluid shadow-4"
-                            src="http://www.diamondpearlz.com/wp-content/uploads/2015/12/banner-1.jpg"                            class="d-block w-100"
+                            src="http://www.diamondpearlz.com/wp-content/uploads/2015/12/banner-1.jpg"                            
+                            class="d-block w-100"
                             alt="First slide"
                         />
                     </div>
                     <Carousel.Caption>
-                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }}>React-Bootstrap</h3>
+                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }}>VENTA</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -31,7 +32,7 @@ const Home = () => {
                         />
                     </div>
                     <Carousel.Caption>
-                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }} >JavaScript</h3>
+                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }} >MANTENIMIENTO</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
@@ -39,39 +40,20 @@ const Home = () => {
                     <div class="col text-center">
                         <img
                             className="img-fluid shadow-4 "
-                            src="https://pbs.twimg.com/media/FAB56EBVUBAvYBb.png"                            class="d-block w-100"
+                            src="https://la-pajarita.es/wp-content/uploads/productos-sostenibles-para-hosteleria-ecofriendly-lapajarita-mapelor.jpg"                            
+                            class="d-block w-100"
+                            width={721}
                             alt="Third slide"
                         />
                     </div>
                     <Carousel.Caption>
-                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }}>Lavarel</h3>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item interval={1000}>
-                    <div class="col text-center">
-                        <img
-                            className="img-fluid shadow-4"
-                            src="https://pngimg.com/uploads/electric_car/electric_car_PNG53.png"                            class="d-block w-100"
-                            alt="Fourth slide"
-                        />
-                    </div>
-                    <Carousel.Caption>
-                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(255,255,255, .2)", backdropFilter: "blur(10px)" }}>Git Control Version</h3>
+                        <h3 style={{ textAlign: "center", backgroundColor: "rgba(0,0,0, .3)", backdropFilter: "blur(10px)" }}>CUIDANDO EL PLANETA</h3>
                     </Carousel.Caption>
                 </Carousel.Item>
 
             </Carousel>
 
             <div>
-                <br></br>
-                
-                <h2 style={{ textAlign: "center" }}><b>Desarrolladores Web:</b>
-                </h2>
-                <h4 style={{ textAlign: "center" }}>Gutiérrez Hernández Luis Fernando
-                </h4>
-                <h4 style={{ textAlign: "center" }}>Hernández Zavala Cassandra Celheotl
-                </h4>
                 <br></br>
             </div>
             <HomeProducts></HomeProducts>
