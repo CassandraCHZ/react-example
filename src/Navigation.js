f433e1b190adef042f3fe7b519dfb3a043de5f55
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-11-27 14:06:59 -0600

    colores brand

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,8 +7,8 @@ const Navigation = () => {
         <div clas="container-fluid">
             <Navbar collapseOnSelect expand="lg" variant="dark" style={{ backgroundColor: "#e3f2fd" }}>
                 <Container >
-                    <Navbar.Brand as={Link} to="/react-example/Home"><h4 style={{color:"#000080"}} >Bienvenida</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Login"><h4 style={{color:"#0AA5AD"}} >Inicio sesión</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Home"><h4 style={{color:"#00AAFF"}} >Bienvenida</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Login"><h4 style={{color:"#00AAFF"}} >Inicio sesión</h4></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
 
