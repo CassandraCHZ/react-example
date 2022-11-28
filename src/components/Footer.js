3fb152457384625ae221839baa6c0e08da163937
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-11-28 00:13:48 -0600

    imagens banner listas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
                         <div style={{ textAlign: "center" }}>
                             <marquee direction="left" SCROLLDELAY="30">
                                 <img
-                                    src="react-example/Images/carro.png"
+                                    src="/react-example/Images/carro.png"
                                     title="Logo"
                                     alt="Logo del Proyecto"
                                     width="130"
@@ -27,7 +27,7 @@ const Footer = () => {
                                     target="_blank"
                                     style={{ marginRight: 35 + "px" }}>
                                     <img
-                                        src="react-example/Images/gmail.png"
+                                        src="/react-example/Images/gmail.png"
                                         title="Gmail"
                                         alt="Gmail"
                                         width="45"
@@ -38,7 +38,7 @@ const Footer = () => {
                                     title="facebook"
                                     target="_blank">
                                     <img
-                                        src="react-example/Images/facebook.png"
+                                        src="/react-example/Images/facebook.png"
                                         title="Facebook"
                                         alt="Facebook"
                                         width="45"
@@ -59,27 +59,6 @@ const Footer = () => {
                     }} >Somos un negocio electrónico especializado en la venta de carros modificados con motores eléctricos, los cuales
                         ofrecen una autonomía de 250 kilómetros, además visto desde el cuidado del ambiente, estos vehículos generan menos emisiones de CO2.
                     </p>
-
-                <div>
-                    <ul class="list-group" style={{ textAlign: "center" }}>
-                        <li class="list-group-item bg-transparent border-0 p-0 mb-2"
-                            style={{ fontSize: "20" + "px" }}>
-                            <img
-                                src="logo192.png"
-                                title="cookies"
-                                alt="Coookies"
-                                width="35"
-                                height="35" 
-                                />
-                            <a
-                                href="http://www.google.com/intl/es-419/policies/technologies/types/"
-                                title="gmail"
-                                target="_blank"><i
-                                    class="fa fa-google-plus mr-1"></i>Cookies</a>
-
-                        </li>
-                    </ul>
-                </div>
                 <div>
                     <ul class="list-group" style={{ textAlign: "center" }}>
                         <li class="list-group-item bg-transparent border-0 p-0 mb-2"
