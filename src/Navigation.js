17c341f95d79e7bc87d54716942677ef60650598
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-11-27 23:29:19 -0600

    navbar fixed

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap'
 
+//'linear-gradient(#AADEE0, #FFFFFF)' #0044FF"
+
 const Navigation = () => {
     return (
         <div clas="container-fluid">
-            <Navbar collapseOnSelect expand="lg" variant="dark" 
-            
-            style={{ background: 'linear-gradient(#AADEE0, #FFFFFF)' }}>
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" 
+            style={{ background: 'linear-gradient(#ebf4fa, #FFFFFF)'  }}>
                 <Container >
-                    <Navbar.Brand as={Link} to="/react-example/Home"><h4 style={{color:"#0099FF"}} >Bienvenida</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Login"><h4 style={{color:"#0099FF"}} >Inicio sesión</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Home"><h4 style={{color:"#006655"}} >Bienvenida</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Login"><h4 style={{color:"#006655"}} >Inicio sesión</h4></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
 
