1c3ce107b6565e12bd68dfe839dd1c1d14184359
CassandraHZ <19151765@aguascalientes.tecnm.mx>
2022-11-28 20:52:33 -0600

    Puse logo y titulo de la marca

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,11 +7,15 @@ import { Navbar, Container } from 'react-bootstrap'
 const Navigation = () => {
     return (
         <div clas="container-fluid">
-            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" 
-            style={{ background: 'linear-gradient(#ebf4fa, #FFFFFF)'  }}>
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark"
+                style={{ background: 'linear-gradient(#ebf4fa, #FFFFFF)' }}>
                 <Container >
-                    <Navbar.Brand as={Link} to="/react-example/Home" title="Bienvenida"><h4 style={{color:"#006655"}} >Bienvenida</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Login" title="Inicio sesión"><h4 style={{color:"#006655"}} >Inicio sesión</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Home" title="Logo"> 
+                        <img src="/react-example/Images/carro.png" title="Logo" alt="Logo del proyecto" width="50" height="44" />
+                    </Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Home" title="Electricar"><h4 style={{ color: "#006655" }} >Electricar</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Home" title="Bienvenida"><h4 style={{ color: "#006655" }} >Bienvenida</h4></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/react-example/Login" title="Inicio sesión"><h4 style={{ color: "#006655" }} >Inicio sesión</h4></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
 
