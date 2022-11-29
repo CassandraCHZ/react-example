a591cf547edce0f640657fd69dbabe1e68b6e37c
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-11-28 23:04:23 -0600

    Cookies mas info arreglado

--- src/components/Cookies.js
+++ src/components/Cookies.js
@@ -8,18 +8,22 @@ const Cookies = () => {
     const alertClose = () => setShowAlert(false);
     return (
         <Modal show={showAlert} onHide={alertClose} >
-            <Modal.Header closeButton style={{background: "#303333", color:"#FFFFFF"}}>
+            <Modal.Header closeButton style={{ background: "#303333", color: "#FFFFFF" }}>
                 <Modal.Title>Cookies de Electricar </Modal.Title>
             </Modal.Header>
-            <Modal.Body style={{background: "#303333", color:"#FFFFFF"}} >
-                    <h6>Usamos cookies propias y de terceros que entre otras cosas recogen datos sobre sus hábitos de navegación para
+            <Modal.Body style={{ background: "#303333", color: "#FFFFFF" }} >
+                <h6>Usamos cookies propias y de terceros que entre otras cosas recogen datos sobre sus hábitos de navegación para
                     mostrarle publicidad personalizada y realizar análisis de uso de nuestro sitio.
                     <br />
                     Si continúa navegando consideramos que acepta su uso.</h6>
             </Modal.Body>
-            <Modal.Footer style={{background: "#303333", color:"#FFFFFF"}}>
+            <Modal.Footer style={{ background: "#303333", color: "#FFFFFF" }}>
                 <Button variant="primary" className="bg-okay" onClick={alertClose} >Aceptar</Button>
-                <Button variant="primary" className="bg-okay" onClick={alertClose} >Más información</Button>
+                <Button variant="primary" className="bg-okay" onClick={alertClose}
+                    href="http://www.google.com/intl/es-419/policies/technologies/types/"
+                    alt="Cookies"
+                    title="Link Cookies"> Más información
+                </Button>
             </Modal.Footer>
         </Modal>
     );
