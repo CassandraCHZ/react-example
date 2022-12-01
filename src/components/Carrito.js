e78eaa8e67d20d2b5c10317d924ac7ff46b3fe10
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-01 12:16:38 -0600

    Carrito creado

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import "../css-components/home-product.css";
+import ReactDOM from "react-dom"
+const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
+
 //json file
 import Autos from "../autos.json";
 
-const Carrito = () => {
-    return (
-        <Container>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h1>Carrito</h1>
-            
-        </Container>
-    );
+class Carrito extends React.Component {
+    createOrder(data, actions) {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: "105",
+                    },
+                },
+            ],
+        });
+    }
+    onApprove(data, actions) {
+        return actions.order.capture();
+    }
+    //apartir de aqui van las funciones
+    render() {
+        return (
+            <Container fluid>
+                <br></br>
+                <br></br>
+                <br></br>
+                <h1>Carrito</h1>
+                
+                <PayPalButton
+                    createOrder={(data, actions) => this.createOrder(data, actions)}
+                    onApprove={(data, actions) => this.onApprove(data, actions)}
+                />
+            </Container>
 
+        );
+    }
 }
 
 export default Carrito;
