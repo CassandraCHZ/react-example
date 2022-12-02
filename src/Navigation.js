e26137e02a48f78eb67a6cb6490d8a9385fe3951
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-02 13:26:31 -0600

    nav bar arreglada responsiva

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,18 +7,22 @@ import { Navbar, Container } from 'react-bootstrap'
 const Navigation = () => {
     return (
         <div clas="container-fluid">
-            <Navbar fixed="top" collapseOnSelect expand="lg" variant="dark"
+            <Navbar fixed="top" collapseOnSelect expand="lg" variant="light"
                 style={{ background: 'linear-gradient(#ebf4fa, #FFFFFF)' }}>
                 <Container >
-                    <Navbar.Brand as={Link} to="/react-example/Home" title="Logo"> 
+                    <Navbar.Brand as={Link} to="/react-example/Home" title="Logo">
                         <img src="/react-example/Images/carro.png" title="Logo" alt="Logo del proyecto" width="50" height="44" />
                     </Navbar.Brand>
                     <Navbar.Brand as={Link} to="/react-example/Home" title="Electricar"><h4 style={{ color: "#006655" }} >Electricar</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Home" title="Bienvenida"><h4 style={{ color: "#006655" }} >Bienvenida</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Login" title="Inicio sesión"><h4 style={{ color: "#006655" }} >Inicio sesión</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Carrito" title="Carrito"><h4 style={{ color: "#006655" }} >Carrito</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Administracion" title="Administracion"><h4 style={{ color: "#006655" }} >Administracion</h4></Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/VistaProducto" title="VistaProdcuto"><h4 style={{ color: "#006655" }} >Vista de Producto</h4></Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Navbar.Brand as={Link} to="/react-example/Login" title="Inicio sesión"><h4 style={{ color: "#006655" }} >Inicio sesión</h4></Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/react-example/Carrito" title="Carrito"><h4 style={{ color: "#006655" }} >Carrito</h4></Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/react-example/Administracion" title="Administracion"><h4 style={{ color: "#006655" }} >Administracion</h4></Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/react-example/VistaProducto" title="VistaProdcuto"><h4 style={{ color: "#006655" }} >Vista de Producto</h4></Navbar.Brand>
+
+                    </Navbar.Collapse>
+
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
 
