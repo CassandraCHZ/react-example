7fcc0e0997d7d8aab3d07522bc70a34fa585c5b3
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-03 14:58:44 -0600

    NAvigation 90

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,10 +10,11 @@ const Navigation = () => {
             <Navbar fixed="top" collapseOnSelect expand="lg" variant="light"
                 style={{ background: 'linear-gradient(#ebf4fa, #FFFFFF)' }}>
                 <Container >
-                    <Navbar.Brand as={Link} to="/react-example/Home" title="Logo">
-                        <img src="/react-example/Images/carro.png" title="Logo" alt="Logo del proyecto" width="50" height="44" />
+                    <Navbar.Brand  style={{ color: "#006655",fontSize:18+"pt" }} as={Link} to="/react-example/Home" title="Logo">
+                        <img src="/react-example/Images/carro.png" title="Logo" alt="Logo del proyecto" width="50" height="44" />Electricar
                     </Navbar.Brand>
-                    <Navbar.Brand as={Link} to="/react-example/Home" title="Electricar"><h4 style={{ color: "#006655" }} >Electricar</h4></Navbar.Brand>
+        
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Navbar.Brand as={Link} to="/react-example/Login" title="Inicio sesión"><h4 style={{ color: "#006655" }} >Inicio sesión</h4></Navbar.Brand>
                         <Navbar.Brand as={Link} to="/react-example/ShoppingCart" title="Carrito"><h4 style={{ color: "#006655" }} >Carrito</h4></Navbar.Brand>
@@ -24,7 +25,7 @@ const Navigation = () => {
                     </Navbar.Collapse>
 
 
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    
                     <Navbar.Collapse id="responsive-navbar-nav">
 
 
