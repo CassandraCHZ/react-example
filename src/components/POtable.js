e606ca08e73f07544da0a2f6208a88fec96d6c23
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-03 17:33:54 -0600

    Purchase 70

--- src/components/POtable.js
+++ src/components/POtable.js
@@ -1,30 +1,102 @@
 import React, { useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Modal, Table } from 'react-bootstrap';
 export default function POtable() {
+    const [showAlert, setShowAlert] = useState(false);
+    const alertClose = () => setShowAlert(false);
+    const alertShow = () => setShowAlert(true);
+
+    const [Pedido1, setPedido1] = useState({
+        codigo: "YMTRYU90",
+        fecha: "2022/11/03",
+        precio: 590000,
+        metodo: "Transferencia",
+        estatus: "Entregado",
+        factura: "No",
+    })
+    const [Pedido2, setPedido2] = useState({
+        codigo: "AATRYU95",
+        fecha: "2022/11/03",
+        precio: 590000,
+        metodo: "Paypal",
+        estatus: "En Proceso",
+        factura: "Sí",
+    })
+
+    const VerDetalles = (e) => {
+        if (e && e.preventDefault()) e.preventDefault();
+        alertShow();
+    }
     return (
-        <Table striped bordered hover variant="ligth"
-            style={{
-                background: "#a5edff",
-                borderColor: "#006655"
-            }}>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Producto</th>
-                    <th>Cantidad</th>
-                    <th>Precio Unitario</th>
-                    <th>Eliminar</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>a</td>
-                    <td>2</td>
-                    <td>g</td>
-                    <td><Button variant="danger" > Eliminar </Button></td>
-                </tr>
-            </tbody>
-        </Table>
+        <>
+            <Table striped bordered hover variant="ligth"
+                style={{
+                    background: "#a5edff",
+                    borderColor: "gray"
+                }}>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Código de pedido</th>
+                        <th>Fecha</th>
+                        <th>Precio Total(MXN)</th>
+                        <th>Método de Pago</th>
+                        <th>Estatus</th>
+                        <th>Factura</th>
+                        <th>Detalles</th>
+                    </tr>
+                </thead>
+                <tbody style={{ justifyContent: "center" }}>
+                    <tr>
+                        <td>1</td>
+                        <td>{Pedido1.codigo}</td>
+                        <td>{Pedido1.fecha}</td>
+                        <td>{Pedido1.precio * 1.16}</td>
+                        <td>{Pedido1.metodo}</td>
+
+                        <td>
+                            <div style={{
+                                background: "#14A44D",
+                                borderRadius: 5 + "px",
+                                color: "white"
+                            }}>{Pedido1.estatus}
+                            </div>
+                        </td>
+                        <td>Pedido1.factura</td>
+                        <td><Button onClick={VerDetalles} variant="link" style={{ color: "blue" }}>Detalles</Button></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>{Pedido2.codigo}</td>
+                        <td>{Pedido2.fecha}</td>
+                        <td>{Pedido2.precio * 1.16}</td>
+                        <td>{Pedido2.metodo}</td>
+
+                        <td>
+                            <div style={{
+                                background: "#14A44D",
+                                borderRadius: 5 + "px",
+                                color: "white"
+                            }}>{Pedido2.estatus}
+                            </div>
+                        </td>
+                        <td>Pedido2.factura</td>
+                        <td><Button onClick={VerDetalles} variant="link" style={{ color: "blue" }}>Detalles</Button></td>
+                    </tr>
+                </tbody>
+            </Table>
+            <Modal show={showAlert} onHide={alertClose}>
+                <Modal.Header closeButton style={{
+                    background: "#a5edff"
+                }} >
+                    <Modal.Title>Detalles del pedido: {Pedido1.codigo}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Agradecemos todos tus comentarios. Responderemos a la mayor brevedad posible.</h4>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={alertClose}>Cerrar</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
--- src/components/PurchaseOrder.js
+++ src/components/PurchaseOrder.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Container, Form, Button, Row, Col, Modal, Table } from 'react-bootstrap';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import POmarque from './POmarque';
 import POtable from './POtable';
 
@@ -7,24 +7,7 @@ import POtable from './POtable';
 import "../css-components/contact-style.css";
 
 export default function Purchase() {
-    const [Contacto, setContacto] = useState({
-        mensaje: ''
-    });
 
-    const [showAlert, setShowAlert] = useState(false);
-    const alertClose = () => setShowAlert(false);
-    const alertShow = () => setShowAlert(true);
-
-    const onChange = (e) => {
-        e.persist();
-        setContacto({ ...Contacto, [e.target.name]: e.target.value });
-    };
-
-    const sendMessage = (e) => {
-        if (e && e.preventDefault()) e.preventDefault();
-        alertShow();
-        Contacto.mensaje = "";
-    }
     return (
         <Container>
             <br></br>
@@ -43,24 +26,8 @@ export default function Purchase() {
                         <h5><b>Aquí están tus pedidos desde la creacion de tu cuenta.</b></h5>
                     </div>
                     <POtable></POtable>
-
                 </Col>
             </Row>
-
-            <Modal show={showAlert} onHide={alertClose}>
-                <Modal.Header style={{
-                    background: "#a5edff"
-                }} closeButton>
-                    <Modal.Title>Mensaje enviado</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h4>Agradecemos todos tus comentarios. Responderemos a la mayor brevedad posible.</h4>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={alertClose}>Cerrar</Button>
-                </Modal.Footer>
-            </Modal>
-
         </Container >
     );
 }
