358e41239ccffad2233a56e49401a4b14f488e3a
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-03 15:54:56 -0600

    MArquePO agregado

---
+++ src/components/MarquePO.js
@@ -0,0 +1,34 @@
+export default function MarquePO() {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <marquee direction="left" SCROLLDELAY="30">
+                <h3 style={{ color: "#006655" }} >Responsabilidad</h3>
+            </marquee>
+            <marquee behavior="scroll" direction="right" scrollamount="15">
+                <h3 style={{ color: "#006655" }} >Innovación</h3>
+            </marquee>
+            <marquee behavior="scroll" direction="up" scrollamount="3">
+                <h3 style={{ color: "#006655" }} >Confianza</h3>
+            </marquee>
+            <marquee behavior="scroll" direction="left" scrollamount="12">
+                <h1 style={{ color: "#006655",fontSize:68+"pt" }} ><img
+                    src="/react-example/Images/carro.png"
+                    title="Logo"
+                    alt="Logo del Proyecto"
+                    width="130"
+                    height="118" />Electricar</h1>
+                
+            </marquee>
+            <marquee behavior="scroll" direction="right" scrollamount="15">
+                <h3 style={{ color: "#006655" }} >Puntualidad</h3>
+            </marquee>
+            <marquee style={{ textAlign: "right" }} behavior="scroll" direction="down" scrollamount="3">
+                <h3 style={{ color: "#006655" }} >Profesionalidad</h3>
+            </marquee>
+
+            <marquee behavior="scroll" direction="left" scrollamount="15">
+                <h3 style={{ color: "#006655" }}>Calidad</h3>
+            </marquee>
+        </div>
+    );
+}
---
+++ src/components/PurchaseOrder.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import { Container, Form, Button, Row, Col, Modal } from 'react-bootstrap';
+import MarquePO from './MarquePO';
+
+//style
+import "../css-components/contact-style.css";
+
+export default function Purchase() {
+    const [Contacto, setContacto] = useState({
+        mensaje: ''
+    });
+
+    const [showAlert, setShowAlert] = useState(false);
+    const alertClose = () => setShowAlert(false);
+    const alertShow = () => setShowAlert(true);
+
+    const onChange = (e) => {
+        e.persist();
+        setContacto({ ...Contacto, [e.target.name]: e.target.value });
+    };
+
+    const sendMessage = (e) => {
+        if (e && e.preventDefault()) e.preventDefault();
+        alertShow();
+        Contacto.mensaje = "";
+    }
+    return (
+        <Container>
+            <br></br>
+            <br></br>
+            <br></br>
+            <Row style={{ justifyContent: 'left' }} >
+                <Col >
+                    <br></br>
+                    <MarquePO></MarquePO>
+                </Col>
+                <Col md={5}>
+                    <br></br>
+                    <br></br>
+                    <h1>Ordenes de Compra</h1>
+                    <h5>Estimado usuario, dejamos a tu disposición este canal de comunicación, para que puedas comunicarte con nosotros.
+                        <br />Estamos a tu servicio 24/7.</h5>
+                    <Form onSubmit={sendMessage} style={{ textAlign: "left" }}>
+                        <br></br>
+                        <Form.Label><h4>Escríbenos...</h4></Form.Label>
+                        <br></br>
+                        <Form.Group className="mb-3">
+                            <Form.Label><b>Para:</b></Form.Label>
+                            <Form.Control placeholder="electricar19151653@gmail.com" disabled />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="mensaje">
+                            <Form.Label><b>Mensaje:</b></Form.Label>
+                            <textarea
+                                required
+                                name="mensaje"
+                                value={Contacto.mensaje}
+                                onChange={onChange}
+                                type="text"
+                                maxLength="250"
+                                minLength={15}
+                                class="text-area-border" rows="5" cols="60"
+                                placeholder="Escribe aquí...">
+                            </textarea>
+                        </Form.Group>
+                        <Button className='buttonSeeProducts' type='submit'><h6>Enviar mensaje</h6>  </Button>
+                        <br></br>
+                    </Form>
+                </Col>
+            </Row>
+
+            <Modal show={showAlert} onHide={alertClose}>
+                <Modal.Header style={{
+                    background: "#a5edff"
+                }} closeButton>
+                    <Modal.Title>Mensaje enviado</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Agradecemos todos tus comentarios. Responderemos a la mayor brevedad posible.</h4>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={alertClose}>Cerrar</Button>
+                </Modal.Footer>
+            </Modal>
+
+        </Container>
+    );
+}
