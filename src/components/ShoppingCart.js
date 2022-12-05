bd57bb9467d837f7949e36ac81c8b163fb6e81ab
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-04 19:32:23 -0600

    Shopping CART 100

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Card, Button, CardGroup, Row, Col, Table, InputGroup, Form, CloseButton } from 'react-bootstrap'; import "../css-components/home-product.css";
 
 import PayPal from "./PayPal";
@@ -8,14 +8,26 @@ import Paypal from './PayPal';
 
 function Carrito() {
     const [carros, setCarros] = useState([Autos.autos[0], Autos.autos[1], Autos.autos[2]]);
-    const [total, settotal] = useState(
-        carros[0].precio + carros[1].precio + carros[2].precio
-    );
+    const [total, settotal] = useState('');
 
-    const eliminarP = (producto) => {
-        console.log("hola")
-        console.log("producto"+producto);
+    useEffect(() => {
+        pago();
+      });
+    const pago = () => {
+        let pay = (carros.reduce((acumulador, actual) => acumulador + actual.precio, 0))*1.16;
+        console.log(pay)
+        settotal(pay);
     }
+
+    function removeObjectWithId(arr, id) {
+        // Making a copy with the Array from() method
+        const arrCopy = Array.from(carros);
+        const objWithIdIndex = arrCopy.findIndex((obj) => obj.id === id);
+        arrCopy.splice(objWithIdIndex, 1);
+        setCarros(arrCopy);
+        return arrCopy;
+    }
+
     return (
         <Container fluid>
             <br></br>
@@ -43,10 +55,16 @@ function Carrito() {
                                             </b></td>
                                             <td>
                                                 <InputGroup className="mb-3">
-                                                    <Button variant="outline-danger"><b>-</b></Button>
-                                                    <Form.Control class="form-control-sm" style={{ maxWidth: 40 + "px" }}></Form.Control>
-                                                    <Button variant="outline-success" ><b>+</b></Button>
-                                                    <CloseButton variant='black' onClick={eliminarP}></CloseButton>
+                                                    <CloseButton variant='black'
+                                                        onClick={() => {
+                                                            console.log("HoLa" + producto.modelo);
+                                                            let car = carros.find(carro => carro.modelo === producto.modelo);
+                                                            console.log(car);
+                                                            const newArr = removeObjectWithId(carros, producto.id);
+                                                        }
+
+                                                        }>
+                                                    </CloseButton>
                                                 </InputGroup>
                                             </td>
                                         </tr>
@@ -59,7 +77,7 @@ function Carrito() {
                 </Col>
 
                 <Col xs lg={4}>
-                    <h1>Total a pagar: <br></br>$ {total}.00 mxn</h1>
+                    <h1>Total a pagar con IVA:<br></br>$ {total}.00 mxn</h1>
                     <br></br>
                     <PayPal total={total}></PayPal>
                 </Col>
@@ -70,3 +88,10 @@ function Carrito() {
     );
 }
 export default Carrito;
+
+/*
+botnes perrones mas y menos cantidad
+<Button variant="outline-danger"><b>-</b></Button>
+                                                    <Form.Control class="form-control-sm" style={{ maxWidth: 40 + "px" }}></Form.Control>
+                                                    <Button variant="outline-success" ><b>+</b></Button>
+*/
