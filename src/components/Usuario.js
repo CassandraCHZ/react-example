16427f4260930203f222e0929e312309b15ad008
mascorroSaul <smascorro2001@gmail.com>
2022-12-05 16:18:32 -0600

    ajustes users mascorroSaul

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -81,7 +81,7 @@ export default function Usuario() {
                                     <td>{user.email}</td>
                                     <td>{user.contraseña}</td>
                                     <td>{user.fechaNacimiento}</td>
-                                    <td><button className="btn btn-danger" onClick={() => deleteTest(user.id)}>Eliminar</button></td>
+                                    <td><button className="btn btn-danger" onClick={() => deleteTest(user.id)}>Borrar</button></td>
                                     <td><button className="btn btn-primary" onClick={() => dataTransfer(user)}>Editar</button></td>
                                 </tr>
                             })
