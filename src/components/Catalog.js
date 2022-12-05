029efa19798aa12819ab2877cc5284ebc0fd2833
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-05 15:58:07 -0600

    Catalog 100

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import "../css-components/home-product.css";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { useNavigate } from 'react-router-dom';
+
 
 
 
@@ -34,6 +32,7 @@ const Catalog = () => {
 
     const [mayorMenorCar, setMayorMenorCar] = useState([Autos.autos[6], Autos.autos[9], Autos.autos[5], Autos.autos[3], Autos.autos[0], Autos.autos[7], Autos.autos[4], Autos.autos[2], Autos.autos[1], Autos.autos[8]],);
 
+
     const selectMenorMayor = () => {
         setAutosOriginales(menorMayorCar);
     }
@@ -54,15 +53,21 @@ const Catalog = () => {
         setAutosOriginales(automoviles);
     }
     //buscar
-    const [buscarPalabra, SetBuscarPalabra] = useState({ palabra: 'buscar' });
+    const [buscarPalabra, SetBuscarPalabra] = useState({ palabra: '' });
 
     const buscar = () => {
-        console.log("buscando" + buscarPalabra.palabra + " hola");
+        console.log("buscando:" + buscarPalabra.palabra);
+        const arrCopy = Array.from(autosTodos);
+        const objWithIdIndex = arrCopy.findIndex((obj) => obj.modelo === buscarPalabra.palabra);
+        console.log("Posicion:" + objWithIdIndex);
+        console.log(Autos.autos[objWithIdIndex]);
+        setAutosOriginales([Autos.autos[objWithIdIndex]]);
     }
     const onChangePalabra = (e) => {
         e.persist();
         SetBuscarPalabra({ ...buscarPalabra, [e.target.name]: e.target.value });
     };
+
 
     return (
 
@@ -74,36 +79,30 @@ const Catalog = () => {
             <h1 class="text-center">Catálogo</h1>
             <br></br>
 
-            <div class="row">
-
-                <div class="col-md-6">
-                </div>
-
-                <div class="col-md-1">
-                    <DropdownButton id="dropdown-item-button" title="Ordenar" variant="outline-primary">
-                        <Dropdown.Item as="button" onClick={selectTodos}>Todos</Dropdown.Item>
-                        <Dropdown.Item as="button" onClick={selectAutomoviles}>Automóvil</Dropdown.Item>
-                        <Dropdown.Item as="button" onClick={selectCamionetas}>Camioneta</Dropdown.Item>
-                        <Dropdown.Item as="button" onClick={selectMenorMayor} >Menor precio</Dropdown.Item>
-                        <Dropdown.Item as="button" onClick={selectMayorMenor}>Mayor precio </Dropdown.Item>
-                    </DropdownButton>
-                </div>
-                <div class="col-md-4">
-
-                    <InputGroup className="mb-3" >
-                        <Button variant="primary" onClick={buscar} style={{ marginLeft: "40px" }}>Buscar</Button>
-                        <Form.Control
-                            type="text"
-                            onChange={onChangePalabra}
-                            placeholder="Buscar producto..."
-                            aria-label="Large"
-                            aria-describedby="inputGroup-sizing-sm"
-                        />
-                    </InputGroup>
-
-
-                </div>
-            </div>
+            <Row>
+                <Form>
+                    <Row style={{textAlign:"right"}}  >
+                        <DropdownButton style={{ maxWidth: 200 + "px" }} id="dropdown-item-button" title="Ordenar" variant="outline-primary">
+                            <Dropdown.Item as="button" onClick={selectTodos}>Todos</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={selectAutomoviles}>Automóvil</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={selectCamionetas}>Camioneta</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={selectMenorMayor} >Menor precio</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={selectMayorMenor}>Mayor precio </Dropdown.Item>
+                        </DropdownButton>
+                        <Button variant="dark" onClick={buscar} style={{ marginLeft: "0px", maxWidth: 80 + "px" }}>Buscar</Button>
+                        <Form.Group style={{ maxWidth: 200 + "px"}} controlId="buscar" >
+                            <Form.Control
+                                type="text"
+                                value={buscarPalabra.palabra}
+                                onChange={onChangePalabra}
+                                placeholder="Buscar producto..."
+                                name="palabra"
+                            />
+                        </Form.Group>
+                    </Row>
+                </Form>
+            </Row>
+            <br></br>
             <div class="row row-cols-1 row-cols-md-4 g-4">
 
                 {
