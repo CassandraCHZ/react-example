8235638f8b127cb42154ff38bd22cb6d67c6ddc2
mascorroSaul <smascorro2001@gmail.com>
2022-12-05 14:00:29 -0600

    función eliminar truck mascorroSaul

--- src/components/TruckCategory.js
+++ src/components/TruckCategory.js
@@ -18,6 +18,29 @@ export default function Producto() {
         descripcion: "Te presentamos Nuevo Tiguan, el SUVW que reúne todo lo que tú y tu familia necesitan en seguridad, tecnología y entretenimiento para todos los trayectos juntos.",
     })
 
+    const [trucks, setTruck] = useState([
+        {
+            id: 1,
+            modelo: "Fortuner",
+            descripcion: "Fortuner integra avanzados dispositivos que optimizan tu seguridad y la de tus acompañantes ante diversas condiciones de manejo.",
+        },
+        {
+            id: 2,
+            modelo: "Hillux",
+            descripcion: "La Hilux ha ganado reputación por solidez y confiabilidad excepcionales, incluso durante uso y/o abuso sostenido, y es comúnmente referida como La Camioneta Indestructible"
+        },
+        {
+            id: 3,
+            modelo: "Tiguan",
+            descripcion: "Te presentamos Nuevo Tiguan, el SUVW que reúne todo lo que tú y tu familia necesitan en seguridad, tecnología y entretenimiento para todos los trayectos juntos.",
+        }
+    ]);
+
+    function deleteTest(id){
+        setTruck(trucks.filter(truck=>truck.id!==id))
+    }
+
+    
     return (
         <>
             <caption align="center">Camionetones</caption>
@@ -34,24 +57,19 @@ export default function Producto() {
                     </tr>
                 </thead>
                 <tbody style={{ justifyContent: "center" }}>
-                    <tr>
-                        <td>1</td>
-                        <td>{Truck1.modelo}</td>
-                        <td>{Truck1.descripcion}</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>{Truck2.modelo}</td>
-                        <td>{Truck2.descripcion}</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>{Truck3.modelo}</td>
-                        <td>{Truck3.descripcion}</td>
-                    </tr>
+                    {
+                        trucks.map((truck, i) => {
+                            return <tr>
+                                <td>{truck.id}</td>
+                                <td>{truck.modelo}</td>
+                                <td>{truck.descripcion}</td>
+                                <td><button onClick={()=>deleteTest(truck.id)}>Borrar</button></td>
+                            </tr>
+                        })
+                    }
                 </tbody>
             </Table>
-            
+
         </>
     );
 }
