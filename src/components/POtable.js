07d4ea910e83d8e24dbaef3274a809861ccb7eee
luis19151653MX <19151653@aguascalientes.tecnm.mx>
2022-12-07 20:50:32 -0600

    POtable listo. Ordenes de compra al 100

--- src/components/POtable.js
+++ src/components/POtable.js
@@ -8,11 +8,10 @@ export default function POtable() {
 
     const [Ordenes, setOrdenes] = useState(JSON.parse(sessionStorage.getItem('ordenesDeCompra')));
 
-    const [modalOrden,setModalOrden]=useState({});
+    const [modalOrden, setModalOrden] = useState({});
 
 
-
-    function verDetalles(orden){
+    function verDetalles(orden) {
         console.log(orden.codigo);
         setModalOrden(orden);
         alertShow();
@@ -73,7 +72,73 @@ export default function POtable() {
                     <Modal.Title>Detalles del pedido: {modalOrden.codigo}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body style={{ background: '#DFE9E9' }}>
-                    
+                    <div style={{
+                        background: "white",
+                        color: "white",
+                        padding: "12px",
+                    }}>
+                        <h5><b>Referencia de pedido {modalOrden.codigo} - efectuado el {modalOrden.fecha}</b></h5>
+                    </div>
+
+                    <br></br>
+                    <div style={{
+                        background: "white",
+                        color: "white",
+                        padding: "12px",
+                    }}>
+                        <h5>Paquetería: ESTAFETA</h5>
+                        <h5>Modo de pago: {modalOrden.metodo}</h5>
+                    </div>
+
+                    <br></br>
+                    <div style={{
+                        background: "white",
+                        color: "white",
+                        padding: "12px",
+                    }}>
+                        <h5> <b>Siga su pedido paso a paso </b></h5>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Fecha</th>
+                                    <th>Estatus</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{modalOrden.fecha}</td>
+                                    <td>{modalOrden.estatus}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+
+                    <br></br>
+                    <div style={{
+                        background: "white",
+                        color: "white",
+                        padding: "12px",
+                    }}>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Fecha</th>
+                                    <th>Paquetería</th>
+                                    <th>Peso</th>
+                                    <th>Costo de envío</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{modalOrden.fecha}</td>
+                                    <td>ESTAFETA</td>
+                                    <td>-</td>
+                                    <td>$20.00 MXN</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" onClick={alertClose}>Cerrar</Button>
